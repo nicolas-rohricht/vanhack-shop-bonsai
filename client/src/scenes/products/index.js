ef17b45f400df12766b243b443b1bafba449dea5
Nícolas Michel Rohricht <nicolas.michel@credpago.com>
2019-11-16 03:12:49 -0300

    Starting with animatons

--- client/src/scenes/products/index.js
+++ client/src/scenes/products/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {SafeAreaView, FlatList, StyleSheet, Text } from 'react-native';
-import {Button} from 'react-native';
+import {View, SafeAreaView, FlatList, StyleSheet, Text, Animated } from 'react-native';
+
 import {useQuery} from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+import Button from 'apsl-react-native-button';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import { Container,  DescriptionContainer, ProductContainer, ProductImage, DescriptionTitle, DescriptionText} from './styledComponents';
 import { verticalScale, scale } from '../../../sizes';
+
+const translateX = new Animated.Value(0);
+
+const animatedEvent = Animated.event(
+  [
+    {
+      nativeEvent: {
+        translationX: translateX
+      }
+    }
+  ],
+  {
+    useNativeDriver: true 
+  }
+)
+
+
 
 const GET_PRODUCTS = gql`
   {
@@ -21,19 +41,70 @@ const GET_PRODUCTS = gql`
   }
 `;
 
+function onHandlerStateChange( event ) {
+  console.tron.log('chegou aqui');
+  if (event.nativeEvent.oldState === State.ACTIVE) {
+    let opened = false;
+    const { translationX } = event.nativeEvent;
+
+    offset += translationX;
+
+    if (translationX >= 100) {
+      opened = true;
+    } else {
+      translateX.setValue(offset);
+      translateX.setOffset(0);
+      offset = 0;
+    }
+
+    Animated.timing(translateX, {
+      toValue: opened ? 380 : 0,
+      duration: 200,
+      useNativeDriver: true
+    }).start(() => {
+      offset = opened ? 380 : 0;
+      translateX.setOffset(offset);
+      translateX.setValue(0);
+    });
+    
+
+    //translateY.setOffset(offset);
+    //translateY.setValue(0);
+  }
+}
+
+const LeftActions = () => {
+  return(
+    <View>
+      <Text>Funciona pleaaase!</Text>
+    </View>
+  )
+}
+
 const renderItem = ({item}) => {
+  
   return (
-    <ProductContainer>
-      <ProductImage source={{uri: item.image}} />
-      <DescriptionContainer>
-        <DescriptionTitle>{item && item.name}</DescriptionTitle>
-        <DescriptionText>Size: {item && item.size}</DescriptionText>
-        <DescriptionText>Color: {item && item.color}</DescriptionText>
-        <DescriptionText>Price: {item && item.price}</DescriptionText>
-        <DescriptionText>Description: {item && item.description}</DescriptionText>
-        <Button title="Buy" type="solid" style={ styles.buyButton } />
-      </DescriptionContainer>
-    </ProductContainer>
+    <Swipeable
+      renderLeftActions={LeftActions}
+    >
+      <ProductContainer >
+        <ProductImage source={{uri: item.image}} />
+        <DescriptionContainer>
+          <DescriptionTitle>{item && item.name}</DescriptionTitle>
+          <DescriptionText>Size: {item && item.size}</DescriptionText>
+          <DescriptionText>Color: {item && item.color}</DescriptionText>
+          <DescriptionText>Price: {item && item.price}</DescriptionText>
+          <DescriptionText>Description: {item && item.description}</DescriptionText>
+          <Button
+            onPress={()=>{}}      
+            style={ styles.buyButton } 
+            textStyle={ styles.textStyle }
+          >
+              Buy
+          </Button>
+        </DescriptionContainer>
+      </ProductContainer>
+    </Swipeable>
   );
 };
 
@@ -70,7 +141,14 @@ const styles = StyleSheet.create({
   },
   buyButton: {
     paddingHorizontal: scale(20),
-    color: '#2196f3'
+    backgroundColor: '#2196f3',
+    borderWidth: 0,
+    marginHorizontal: scale(10),
+    marginTop: verticalScale(10)
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: 'white',
   }
 });
 
--- client/src/scenes/products/styledComponents.js
+++ client/src/scenes/products/styledComponents.js
@@ -1,26 +1,29 @@
 import styled from 'styled-components';
+import { Animated } from 'react-native';
 
 export const Container = styled.View`
   background-color: #f4f4f4;
   flex: 1;
+  zIndex: 5;
 `;
 
-export const ProductContainer = styled.View`
+export const ProductContainer = styled(Animated.View)`
   flex: 1;
   flex-direction: row;
   margin: 10px 10px;
-  border-radius: 8px
+  border-radius: 8px;
 `;
 
-export const DescriptionContainer = styled.View`
+export const DescriptionContainer =styled(Animated.View)`
   flex: 1;
   flex-direction: column;
 `;
 
 export const ProductImage = styled.Image`
   width: 130;
-  height: 180;
+  height: 200;
   border-radius: 8px;
+  align-self: center;
 `
 export const DescriptionTitle = styled.Text`
   font-size: 20px;
