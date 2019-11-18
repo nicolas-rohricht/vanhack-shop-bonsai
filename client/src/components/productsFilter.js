73f7b3feef94f8baa4e689b9169155c770e2e61e
Nícolas Michel Rohricht <nicolas.michel@credpago.com>
2019-11-18 15:44:34 -0300

    Making filter even better (:

--- client/src/components/productsFilter.js
+++ client/src/components/productsFilter.js
@@ -35,12 +35,22 @@ export default class ProductsFilter extends Component {
 
     this.setState({ merchants });
   }
+
+  changeSelectedColors = ( item ) => {
+    this.state.colors.map(element => { 
+      if ( item.id === element.id ) {
+        element.selected = !element.selected
+      };
+    });
+
+    this.setState({ colors });
+  }
   
   renderBrandsItems = ( {item} ) => {
     return(
       <TouchableOpacity onPress={ () => { this.changeSelectedBrands( item ) }}>
         <ItemContainer>
-          <ItemInitialsContainer style={{ borderWidth: item.selected ? 4 : 0.6, 
+          <ItemInitialsContainer style={{ borderWidth: item.selected ? 4 : 0, 
                                           borderColor: item.selected ? '#00cc66' : 'black' }} >
             <ItemInitials>{item.initials}</ItemInitials>
           </ItemInitialsContainer>
@@ -54,7 +64,7 @@ export default class ProductsFilter extends Component {
     return(
       <TouchableOpacity onPress={ () => { this.changeSelectedMerchants( item ) }}>
         <ItemContainer>
-          <ItemInitialsContainer style={{ borderWidth: item.selected ? 4 : 0.6, 
+          <ItemInitialsContainer style={{ borderWidth: item.selected ? 4 : 0, 
                                           borderColor: item.selected ? '#00cc66' : 'black' }} >
             <ItemInitials>{item.initials}</ItemInitials>
           </ItemInitialsContainer>
@@ -64,36 +74,79 @@ export default class ProductsFilter extends Component {
     )
   }
 
+  renderColorsItems = ({ item }) => {
+    return(
+      <TouchableOpacity onPress={ () => { this.changeSelectedColors( item ) }}>
+        <ItemContainer>
+          <ItemInitialsContainer 
+            style={{ borderWidth: item.selected ? 4 : 0, 
+                     borderColor: item.selected ? '#00cc66' : 'black' ,
+                     backgroundColor: item.color
+                   }} 
+          >
+          </ItemInitialsContainer>
+          <ItemName style={{ color: item.selected ? '#00cc66' : 'black' }} >{item.name}</ItemName>
+        </ItemContainer>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
-      <ScrollView>
-        <Container>
-          <TitlesContainer>
-            <Title>Are you looking for a specific thing?</Title>
-            <Subtitle>Find here!</Subtitle>
-          </TitlesContainer>
-          <SectionContainer>
-            <SectionTitle>Brands</SectionTitle>
-              <FlatList
-                style={{ alignSelf: "center"}}
-                data={this.state.brands}
-                renderItem={this.renderBrandsItems}
-                keyExtractor={item => item.id.toString()}
-                numColumns={3}
+      <Container>
+        <ScrollView>
+            <TitlesContainer>
+              <Title>Are you looking for a specific thing?</Title>
+              <Subtitle>Find here!</Subtitle>
+            </TitlesContainer>
+            <SectionContainer>
+              <SectionTitle>Product name</SectionTitle>
+              <InputFilter 
+                placeholder='Search here by product name...'
               />
-          </SectionContainer>
-          <SectionContainer>
-            <SectionTitle>Merchants</SectionTitle>
-              <FlatList
-                style={{ alignSelf: "center"}}
-                data={this.state.merchants}
-                renderItem={this.renderMerchantsItems}
-                keyExtractor={item => item.id.toString()}
-                numColumns={3}
-              />
-          </SectionContainer>
-        </Container>
-      </ScrollView>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionTitle>Brands</SectionTitle>
+                <FlatList
+                  style={{ alignSelf: "center"}}
+                  data={this.state.brands}
+                  renderItem={this.renderBrandsItems}
+                  keyExtractor={item => item.id.toString()}
+                  numColumns={3}
+                />
+            </SectionContainer>
+            <SectionContainer>
+              <SectionTitle>Colors</SectionTitle>
+                <FlatList
+                  style={{ alignSelf: "center"}}
+                  data={this.state.colors}
+                  renderItem={this.renderColorsItems}
+                  keyExtractor={item => item.id.toString()}
+                  numColumns={3}
+                />
+            </SectionContainer>
+            <SectionContainer>
+              <SectionTitle>Merchants</SectionTitle>
+                <FlatList
+                  style={{ alignSelf: "center"}}
+                  data={this.state.merchants}
+                  renderItem={this.renderMerchantsItems}
+                  keyExtractor={item => item.id.toString()}
+                  numColumns={3}
+                />
+            </SectionContainer>
+        </ScrollView>
+        <FooterButtonsContainer>
+          <FooterButtons 
+            title='Reset filter'
+          >
+          </FooterButtons>
+          <FooterButtons 
+            title='Aply filter'
+          >
+          </FooterButtons>
+        </FooterButtonsContainer>
+      </Container>
     )
   }
 }
@@ -101,6 +154,7 @@ export default class ProductsFilter extends Component {
 const Container = styled.View`
   flex-direction: column;
   margin: 10px 10px;
+  flex: 1;
 `
 
 const Title = styled.Text`
@@ -148,4 +202,23 @@ const ItemName = styled.Text`
   font-size: 16px;
   text-align: center;
   margin: 5px;
+`
+const InputFilter = styled.TextInput`
+  height: 40px;
+  border-width: 0.4;
+  border-color: gray;
+  border-radius: 8;
+  margin-top: 10px;
+  font-size: 18px;
+`
+const FooterButtonsContainer = styled.View`
+  flex-direction: row;
+  z-index: -1;
+  justify-content: space-between;
+  padding-top: 10px;
+`
+const FooterButtons = styled.View`
+  border-radius: 8;
+  height: 40px;
+  width: 
 `
